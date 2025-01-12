f440e8b30b115d0a94a633948732c56aceebb406
Kartik7906 <kartikvam799@gmail.com>
2025-01-12 15:27:27 +0530

    update intro design

--- Client/src/Components/Task1/Task1.jsx
+++ Client/src/Components/Task1/Task1.jsx
@@ -1,103 +1,64 @@
-import React from 'react'
-import './Task1.css'
+import React from 'react';
+import './Task1.css';
 
 const Task1 = () => {
   const handleStart = () => {
-    console.log('Start Assessment triggered.')
-  }
+    console.log('Start Assessment triggered.');
+  };
 
   const handleStartRecord = () => {
-    console.log('Start Test triggered.')
-  }
+    console.log('Start Test triggered.');
+  };
 
   return (
     <div className="task1-wrapper">
-      {/* Main Box / Assessment Header */}
-      <div className="box">
-        <br />
-        <center>
-          <p className="self">Candidate Assessment</p>
-          <div className="intro"></div>
-        </center>
-        <br />
-        <br />
-        <br />
+      <div className="boost-skills-section">
+        <div className="skills-container">
+          <h1 className="boost-title">Boost Your Skills</h1>
+          <p className="boost-text">
+            Enhance your IT knowledge and expertise with our specialized training programs.
+          </p>
+          <button className="explore-button">Explore</button>
+        </div>
+      </div>
 
-        <div className="steps-container">
-          <div className="steps">
-            <span className="circle active">1</span>
-            <span className="circle">2</span>
-            <span className="circle">3</span>
-            <div className="progress-bar">
-              <span className="indicator"></span>
-            </div>
+      {/* Main Content */}
+      <div className="row">
+        {/* Candidate Assessment Section */}
+        <div className="col">
+          <div className="box">
+            <p className="self">Candidate Assessment</p>
+            <div className="intro"></div>
+          </div>
+        </div>
+
+        {/* Assessment Instructions */}
+        <div className="col">
+          <div className="asses1" id="asses1">
+            <p className="intro_text">
+              Can you give us a brief introduction about yourself in the next 2 minutes? Feel free to cover your personal background, education, work experience, or anything you feel is relevant.
+            </p>
+            <button className="record" id="start" onClick={handleStart}>
+              Start Assessment
+            </button>
           </div>
         </div>
       </div>
 
-      <br />
-      <br />
-      <br />
-
-      {/* Assessment Instructions */}
-      <center>
-        <div className="asses1" id="asses1">
-          <div className="intro_text">
-            <p>
-              Can you give us a brief introduction about yourself in the next 2
-              minutes? Feel free to cover your personal background, education,
-              work experience, or anything you feel is relevant.
-            </p>
-          </div>
-          <br />
-          <br />
-          <button className="record" id="start" onClick={handleStart}>
-            Start Assessment
-          </button>
-          <br />
-          <br />
-        </div>
-
-        {/* Recording Section */}
-        <div id="recordings" className="recordings">
-          <center>
-            <p className="self">Self-Introduction</p>
-            <br />
-            <br />
-            <div className="intro_text">
-              <p>
-                Can you give us a brief introduction about yourself in the next
-                2 minutes? Feel free to cover your personal background,
-                education, work experience, or anything you feel is relevant.
-              </p>
-            </div>
-          </center>
-          <br />
-          <br />
-          <video id="video" className="video" autoPlay muted></video>
-          <br />
-          <br />
-          <div id="timer" className="timer"></div>
-          <br />
-          <button
-            className="record_start"
-            id="start_record"
-            onClick={handleStartRecord}
-          >
-            Start Test
-          </button>
-          <br />
-          <br />
-        </div>
-      </center>
-
-      <br />
-      <br />
-
-      {/* Placeholder for the end or next steps */}
-      <div className="end" id="end"></div>
+      {/* Recording Section */}
+      <div className="recordings" id="recordings">
+        <p className="self">Self-Introduction</p>
+        <p className="intro_text">
+          Can you give us a brief introduction about yourself in the next 2 minutes? Feel free to cover your personal background, education, work experience, or anything you feel is relevant.
+        </p>
+        <video id="video" className="video" autoPlay muted></video>
+        <div id="timer" className="timer"></div>
+        <button className="record_start" id="start_record" onClick={handleStartRecord}>
+          Start Test
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Task1
+export default Task1;
